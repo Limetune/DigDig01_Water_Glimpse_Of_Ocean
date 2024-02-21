cfdb6bc5f815cd6b08cba4718b977283f6e8be5a
ViggoWarnehall <156196617+ViggoWarnehall@users.noreply.github.com>
2024-02-21 12:47:15 +0100

    camera

--- DigDig01GroupWaterGlimpseOfOcean/Assets/Scripts/camerafollow.cs
+++ DigDig01GroupWaterGlimpseOfOcean/Assets/Scripts/camerafollow.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class camerafollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float followspeed = 2f;
+    public Transform target;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, followspeed * Time.deltaTime);
     }
+
+
 }
