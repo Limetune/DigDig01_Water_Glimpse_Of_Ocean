2a65dd6aa3d151b2a82f6a5a7344918b25439d9c
ViggoWarnehall <156196617+ViggoWarnehall@users.noreply.github.com>
2024-03-06 12:55:21 +0100

    halvt klar dash

--- DigDig01GroupWaterGlimpseOfOcean/Assets/Scripts/MovementScriptPlayer1.cs
+++ DigDig01GroupWaterGlimpseOfOcean/Assets/Scripts/MovementScriptPlayer1.cs
@@ -9,8 +9,13 @@ public class PlayerMovementScriptPlayer1 : MonoBehaviour
     public float movementspeed;
     public Rigidbody2D rb;
     Vector2 moveDirection;
-
     bool lookRight = true;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
 
     // Update is called once per frame
     void Update()
@@ -18,6 +23,10 @@ public class PlayerMovementScriptPlayer1 : MonoBehaviour
         float moveX = 0;
         float moveY = 0;
 
+        if (isDashing)
+        {
+            return;
+        }
         if (Input.GetKey(KeyCode.W))
         {
             moveY = 1;
@@ -34,12 +43,19 @@ public class PlayerMovementScriptPlayer1 : MonoBehaviour
         {
             moveX = -1;
         }
-
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
         moveDirection = new Vector2(moveX, moveY).normalized;
         Flip(moveX);
     }
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
         rb.velocity = new Vector2(moveDirection.x * movementspeed, moveDirection.y * movementspeed);
     }
     void Flip(float moveX)
@@ -50,4 +66,17 @@ public class PlayerMovementScriptPlayer1 : MonoBehaviour
             lookRight = !lookRight;
         }
     }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower,
+                                  0f);
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
+
 }
