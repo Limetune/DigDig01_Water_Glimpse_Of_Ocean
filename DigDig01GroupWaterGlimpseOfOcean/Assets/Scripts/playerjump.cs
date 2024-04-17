4c561242b493955365bce2ea27b5459568b23680
Limetune <152399037+Limetune@users.noreply.github.com>
2024-04-17 12:50:12 +0200

    Jump script v4

--- DigDig01GroupWaterGlimpseOfOcean/Assets/Scripts/playerjump.cs
+++ DigDig01GroupWaterGlimpseOfOcean/Assets/Scripts/playerjump.cs
@@ -7,7 +7,45 @@ public class NewBehaviourScript : MonoBehaviour
     private Rigidbody2D rb;
     public float speed = 10f;
     public float force = 300f;
-   
+    float coolDown = 0.5f; // Adjust cooldown time as needed
+    float nextTimeToJump = 0;
+    bool canJump = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && Time.time > nextTimeToJump && canJump)
+        {
+            nextTimeToJump = Time.time + coolDown;
+            rb.AddForce(Vector2.up * rb.mass * force, ForceMode2D.Impulse);
+            canJump = false; // Disable jumping until cooldown is over
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Check if cooldown has elapsed, then allow jumping again
+        if (!canJump && Time.time > nextTimeToJump)
+        {
+            canJump = true;
+        }
+    }
+}
+//  rb.AddForce(new Vector2(rb.velocity.x, jump));
+//  public float jump;
+/* private Rigidbody2D rb;
+    public float speed = 10f;
+    public float force = 300f;
+    float coolDown = 3;
+    float nextTimeToJump = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,12 +64,10 @@ public class NewBehaviourScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (jump)
+        if (jump && nextTimeToJump < Time.time)
         {
             rb.AddForce(Vector2.up * rb.mass * force, ForceMode2D.Impulse);
-            
+            nextTimeToJump = Time.time + coolDown;
         }
     }
-}
-//  rb.AddForce(new Vector2(rb.velocity.x, jump));
-//  public float jump;
+*/
